9044bfa7d6477eafd15b8575dc5b764f1a793035
Gabrieli <gabibrandalise20662@gmail.com>
2025-05-08 19:19:14 -0300

    adição rotasItemPedido em Rotas

--- rota/rotas.js
+++ rota/rotas.js
@@ -4,12 +4,13 @@ const { rotasProduto } = require('./rotasProduto');
 const { rotasCliente } = require('./rotasCliente');
 const { rotasPedido } = require('./rotasPedido');
 const { rotasAplicacao } = require('./rotasAplicacao');
-
+const { rotasItemPedido } = require('./rotasItemPedido');
 const rotas = new Router();
 
 rotas.use(rotasProduto);
 rotas.use(rotasCliente);
 rotas.use(rotasPedido);
 rotas.use(rotasAplicacao);
+rotas.use(rotasItemPedido);
 
 module.exports = rotas;
