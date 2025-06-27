6fb0b1afeeefb759e5a8003ca448be93e1c03efe
Gabrieli <gabibrandalise20662@gmail.com>
2025-06-26 21:48:58 -0300

    alteração rotas usuario

--- rota/rotasUsuario.js
+++ rota/rotasUsuario.js
@@ -1,14 +1,18 @@
 const { Router } = require('express');
-const {login, createAccount, updateUsuario} = require('../controlador/segurancaControlador');
+const {login, createAccount, updateUsuario, getUsuario, verificaJWT} = require('../controlador/segurancaControlador');
 
 const rotasUsuario = new Router();
 
 rotasUsuario.route('/login')
    .post(login)
-   .put(updateUsuario)
 
 rotasUsuario.route('/createaccount')
    .post(createAccount)
-   
 
+rotasUsuario.route('/usuario')
+   .put(verificaJWT, updateUsuario); 
+
+rotasUsuario.route('/usuario/:id')
+   .get(verificaJWT, getUsuario);
+   
 module.exports = { rotasUsuario };
